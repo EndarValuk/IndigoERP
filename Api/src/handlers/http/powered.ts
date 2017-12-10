cd4c80511578ba90ed8e80f3dafc76d18a195003
Dmitry Besedin <endarvaluk@gmail.com>
2017-12-10 23:05:26 +0500

    [*] Minor typo.

--- Api/src/handlers/http/powered.ts
+++ Api/src/handlers/http/powered.ts
@@ -3,7 +3,7 @@
  */
 import { Request, Response, NextFunction } from "express";
 
-const poweredHandler = (eq: Request, res: Response, next: NextFunction) => {
+const poweredHandler = (req: Request, res: Response, next: NextFunction) => {
   // Set our own header here
   res.setHeader('X-Powered-By', 'IndigoERP');
   next();
