6ddc9700854323755d6a0ee611de7530850e16e7
Dmitry Besedin <endarvaluk@gmail.com>
2017-12-12 23:46:48 +0500

    [*] Reworked api envelope for better consistence.

--- Api/src/datasource/models/envelope.ts
+++ Api/src/datasource/models/envelope.ts
@@ -1,5 +1,16 @@
+import { ResultType } from '@indigo/types';
+
 class Envelope<T> {
-  public constructor(public Value: T, public Type: number, public Message?: string) {
+  public Type: ResultType = ResultType.NoResultYet;
+
+  public Value?: T;
+
+  public Message?: string;
+
+  public constructor(resultType?: ResultType, value?: T, message?: string) {
+    this.Type = resultType || ResultType.NoResultYet;
+    this.Value = value;
+    this.Message = message;
   }
 }
 
