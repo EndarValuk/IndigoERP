47470dcf9c23ee010d648165aa6103a3c978c58c
Dmitry Besedin <endarvaluk@gmail.com>
2017-12-14 00:23:18 +0500

    [*] Revamped API result Envelope. Moved error handling to Envelope class.

--- Api/src/datasource/models/envelope.ts
+++ Api/src/datasource/models/envelope.ts
@@ -1,16 +1,48 @@
 import { ResultType } from '@indigo/types';
 
 class Envelope<T> {
-  public Type: ResultType = ResultType.NoResultYet;
+  public readonly Type: ResultType = ResultType.NoResultYet;
 
-  public Value?: T;
+  public readonly Value?: T | Error;
 
-  public Message?: string;
+  public readonly Message?: string;
 
-  public constructor(resultType?: ResultType, value?: T, message?: string) {
-    this.Type = resultType || ResultType.NoResultYet;
+  public readonly IsSuccessful: boolean;
+
+  public constructor(resultType?: ResultType, value?: T | Error, message?: string) {
+    if(resultType) {
+      this.Type = resultType;
+    }
     this.Value = value;
     this.Message = message;
+
+    if(value && value instanceof Error) {
+      switch(value.name) {
+        case "SequelizeHostNotReachableError": {
+          this.Type = ResultType.ErrorDatabaseConnection;
+        }break;
+        case "SequelizeUniqueConstraintError": {
+          this.Type = ResultType.ErrorDatabaseCreateExists;
+        }break;
+        case "SequelizeDatabaseError": {
+          this.Type = ResultType.ErrorDatabase;
+        }break;
+        default: {
+          this.Type = ResultType.Error;
+ 
+          console.log(value.name);
+          console.log(value.message);
+          console.log(value.stack);
+        }break;
+      }
+      this.Value = undefined;
+      this.Message = value.message;
+    }
+
+    let hasError = (this.Type & ResultType.Error) === ResultType.Error;
+    let nyi = (this.Type & ResultType.NYI) === ResultType.NYI;
+    let nyr = (this.Type & ResultType.NoResultYet) === ResultType.NoResultYet;
+    this.IsSuccessful = !hasError && !nyi && !nyr;
   }
 }
 
