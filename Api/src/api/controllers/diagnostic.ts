301fbe01eca83a1e2f33981af22911412c309080
Dmitry Besedin <endarvaluk@gmail.com>
2017-12-14 00:21:25 +0500

    [+] Added diagnostic(Memory, CPU, platform, etc.) API controller.

---
+++ Api/src/api/controllers/diagnostic.ts
@@ -0,0 +1,21 @@
+import * as os from "os";
+import { Controller, Get } from "routing-controllers";
+
+@Controller()
+export class DiagnosticController {
+  /**
+   * Diagnostic request
+   */
+  @Get("/diag")
+  onPost() {
+    let freeMb = Math.floor(os.freemem() / 1024 / 1024);
+    let totalMb = Math.floor(os.totalmem() / 1024 / 1024);
+
+    return {
+      Platform: `${os.platform()} on ${os.arch()} architecture`,
+      Cpus: os.cpus().length,
+      Network: os.networkInterfaces(),
+      Memory: `${freeMb}Mb / ${totalMb}Mb (${Math.floor(os.freemem() / os.totalmem()*100)}% free)`,
+    };
+  }
+}
