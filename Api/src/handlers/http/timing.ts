bee4e57faa8d6dc8a460d97f7eca58cc9ea0e863
Dmitry Besedin <endarvaluk@gmail.com>
2017-12-14 00:27:36 +0500

    [*] Fiexed timing handler.

--- Api/src/handlers/http/timing.ts
+++ Api/src/handlers/http/timing.ts
@@ -1,18 +1,24 @@
-/**
- * Loading dependencies.
- */
+// Loading external dependencies.
 import { Request, Response, NextFunction } from "express";
+// Loading local dependencies.
 import { logger } from '@indigo/handlers';
 
+/**
+ * 
+ * @param req 
+ * @param res 
+ * @param next 
+ */
 const timingHandler = (req: Request, res: Response, next: NextFunction) => {
   // Check start time
   let beginTime = Date.now();
   // Check end time
   res.addListener('finish', () => {
     let d =  Date.now();
-    logger.debug(`Reponse time: ${req.url}: ${(d - beginTime)}`);
+    logger.info(`Reponse time: ${req.url}: ${(d - beginTime)}ms`);
   });
   // Continue to the next handler
   next();
 };
+
 export { timingHandler };
