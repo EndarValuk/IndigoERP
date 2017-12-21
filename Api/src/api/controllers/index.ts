0da3730c06225d0739883b2c4e5adac64822f5ed
Dmitry Besedin <endarvaluk@gmail.com>
2017-12-22 00:11:13 +0500

    [*] Fixed broken import by previous commit.

--- Api/src/api/controllers/index.ts
+++ Api/src/api/controllers/index.ts
@@ -1,6 +1,6 @@
 import { DiagnosticController } from './diagnostic';
 import { EchoController } from './echo';
-import { ObjectActionController } from './core/object_action';
+import { ObjectActionController } from './core/generic_object';
 import { ReferenceController } from './core/reference';
 import { UsersController } from './core/users';
 
